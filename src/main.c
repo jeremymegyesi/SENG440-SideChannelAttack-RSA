3d7660e4976ecad5befa4a6ef792afbc6b57f633
amandeepjohal <amaniejohal@gmail.com>
2021-08-05 20:22:00 -0700

    Replace R factor robustness with subtraction robustness

--- src/main.c
+++ src/main.c
@@ -29,25 +29,23 @@ int count_num_bits(int value) {
 Montgomery Modular Multiplication algorithm
 Returns result of X * Y given N largest key size (modulus)
 */
-uint32_t MMM(uint32_t X, uint32_t Y) {
+uint32_t MMM(uint32_t X, uint32_t Y) { 
 	uint32_t T = 0;
 	uint32_t T_fake = 0;
 	uint64_t m = count_num_bits(N);
-	uint64_t R = 1 << m;
-
+	
 	for ( uint32_t i = 0; i < m; i++ ) {
 		bool nu = (T & 1) ^ ((X & (1 << i)) && (Y & 1));
 		T = (T + ((X & (1 << i)) >> i)*Y + nu*N) >> 1;
 	}
+	
 	if (T >= N) {
 		T = T - N;
+	} else {
+	    T_fake = T - N;
 	}
 	
-	uint64_t remaining_bits = MAX_BIT_SIZE - R;
-	T = T * R % N;
-	T_fake = T_fake * remaining_bits % N; // fake operation for robustness
-	
-	return T;
+	return (T << m) % N;
 }
 
 /*
